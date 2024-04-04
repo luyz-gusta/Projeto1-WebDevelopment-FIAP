a38817ab1e5dcf2f678df87402df20e502f847a2
luyz <luizgustavo.sp2020@gmail.com>
2024-04-04 11:11:07 -0300

    function mudar() criado

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const mudar = () => {
+    const inputNome = document.getElementById('idNome').value
+    const h1Titulo = document.getElementById('idTitulo')
+    h1Titulo.innerHTML = inputNome
+}
