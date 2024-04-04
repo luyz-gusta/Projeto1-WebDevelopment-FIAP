08b14e28ad98b3a60f9735bc7b9d607a22669164
luyz <luizgustavo.sp2020@gmail.com>
2024-04-04 11:40:58 -0300

    Enviando func calcular

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict'
 
-const mudar = () => {
-    const inputNome = document.getElementById('idNome').value
-    const h1Titulo = document.getElementById('idTitulo')
-    h1Titulo.innerHTML = inputNome
+// const mudar = () => {
+//     const inputNome = document.getElementById('idNome').value
+//     const h1Titulo = document.getElementById('idTitulo')
+//     h1Titulo.innerHTML = inputNome
+// }
+
+const mudar = () => document.getElementById('idTitulo').innerHTML = document.getElementById('idNome').value
+
+const calcular = () => {
+    let num1 = parseFloat(document.getElementById("num1").value)
+    let num2 = parseFloat(document.getElementById("num2").value)
+    document.getElementById('resultado').innerHTML = num1 + num2
 }
